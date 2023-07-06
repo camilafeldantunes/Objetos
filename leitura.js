ca493ffa9c759220c452d240a7a92077dee57ef6
Camila Feldkircher Antunes <99697225+camilafeldantunes@users.noreply.github.com>
2023-07-06 09:47:08 -0300

    Aula 4 sobre objetos. Conhecendo JSON

--- leitura.js
+++ leitura.js
@@ -8,8 +8,8 @@ const clienteString = JSON.stringify(dados);
 console.log(clienteString);
 console.log(typeof clienteString);
 
-console.log(dados.email);
 
 const objetoCliente = JSON.parse(clienteString);
 
 console.log(objetoCliente);
+console.log(typeof objetoCliente);
